5a6588883bcc62820f77d5d1a7f0bfc6655cb2cb
Tugay Türk <i.tugayturk@gmail.com>
2020-12-17 20:23:55 +0000

    ödev çözümü

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-function Animal(name){
+class Animal2 {
+  constructor(name, image) {
     this.name = name;
-}
+    this.image = image;
+  }
+  action() {
+    document.getElementById(this.actionSoundName).play();
+  }
 
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
-}
+    putImageInTheDocument() {
+    var image = document.createElement("img");
+    image.src = this.image;
+    image.height = 400;
 
-Animal.prototype.putInTheDocument = function(){
+
+    var imageArea = document.getElementById("image-area");
+    console.log(imageArea);
+    imageArea.innerHTML = "";
+    imageArea.appendChild(image)
+  }
+
+  putInTheDocument() {
     var petsTable = document.getElementById("petsTable");
     var petTR = document.createElement("tr");
-  
+    petTR.onclick = this.putImageInTheDocument.bind(this);
+
     var petNameTD = document.createElement("td");
     petNameTD.textContent = this.name;
     petTR.appendChild(petNameTD);
-  
+
     var petLegsTD = document.createElement("td");
     petLegsTD.textContent = this.legs;
     petTR.appendChild(petLegsTD);
-  
+
     var petActionTD = document.createElement("td");
     var petActionTDButton = document.createElement("button");
     petActionTDButton.textContent = this.actionText;
     petActionTD.appendChild(petActionTDButton);
     petTR.appendChild(petActionTD);
-    
+
     petActionTDButton.onclick = this.action.bind(this);
     petsTable.querySelector("tbody").appendChild(petTR)
+
+  }
 }
 
-
-function Cat(name){
-    Animal.call(this, name);
+class Cat2 extends Animal2 {
+  constructor(name, image) {
+    super(name, image)
     this.legs = 4;
     this.actionText = "Meoow"
     this.actionSoundName = "meow"
+  }
+
 }
-
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
+class Monkey2 extends Animal2 {
+  constructor(name, image) {
+    super(name, image)
     this.legs = 2;
     this.actionText = "Scream";
     this.actionSoundName = "scream";
+  }
 }
 
-Monkey.prototype = Animal.prototype;
-
-var Mila = new Cat("Mila");
+var Mila = new Cat2("Mila", "https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260");
 Mila.putInTheDocument();
 
-var Charlie = new Monkey("Charlie");
+var Charlie = new Monkey2("Charlie", "https://cdn.mos.cms.futurecdn.net/wxLD2KnTXBcrDWSBdaLixK-1200-80.jpg");
 Charlie.putInTheDocument();
+
